5db90ca2b4de9fdc1b93547ff14afe3fac417291
gkhandagale <68633648+gkhandagale@users.noreply.github.com>
2021-05-29 22:26:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default= "jai"
+  default ="TEST"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default="West US 2"
+  default ="US West 2"
 }
